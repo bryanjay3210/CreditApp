ba5a54e01c0cf0a6311874a8acc38a66352de593
bryanjay3210 <78206367+bryanjay3210@users.noreply.github.com>
2023-07-23 02:27:44 +0800

    add welcome name and disable back button on dashboard

--- credit_app/lib/view/home/cubit/home_state.dart
+++ credit_app/lib/view/home/cubit/home_state.dart
@@ -4,19 +4,26 @@ class HomeState extends Equatable {
   final bool isShow;
   final double totalCredit;
   final double totalDebit;
+  final String fullname;
   const HomeState({
     required this.isShow,
     required this.totalCredit,
     required this.totalDebit,
+    required this.fullname,
   });
 
-  HomeState copyWith({bool? isShow, double? totalCredit, double? totalDebit}) {
+  HomeState copyWith(
+      {bool? isShow,
+      double? totalCredit,
+      double? totalDebit,
+      String? fullname}) {
     return HomeState(
         isShow: isShow ?? this.isShow,
         totalCredit: totalCredit ?? this.totalCredit,
-        totalDebit: totalDebit ?? this.totalDebit);
+        totalDebit: totalDebit ?? this.totalDebit,
+        fullname: fullname ?? this.fullname);
   }
 
   @override
-  List<Object> get props => [isShow, totalCredit, totalDebit];
+  List<Object> get props => [isShow, totalCredit, totalDebit, fullname];
 }
--- credit_app/lib/view/home/home.dart
+++ credit_app/lib/view/home/home.dart
@@ -18,6 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     context.read<HomeCubit>().computeTotal();
+    context.read<HomeCubit>().getUser();
     super.initState();
   }
 
@@ -29,156 +30,165 @@ class _HomeScreenState extends State<HomeScreen> {
           // TODO: implement listener
         },
         builder: (context, state) {
-          return Padding(
-            padding: const EdgeInsets.all(20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Container(
-                height: 150,
-                decoration: BoxDecoration(
-                  color: kDefaultColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: const Offset(0, 5), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            'Hi Bryan!',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 23,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          const Spacer(),
-                          IconButton(
-                              onPressed: () {
-                                context.read<HomeCubit>().toogleIsShow();
-                              },
-                              icon: Icon(
-                                state.isShow
-                                    ? Icons.visibility_outlined
-                                    : Icons.visibility_off_outlined,
-                                color: Colors.white,
-                              ))
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        children: [
-                          const Column(
-                            children: [
-                              Text('Total Credit',
-                                  style: TextStyle(color: Colors.white)),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text('Total Debit',
-                                  style: TextStyle(color: Colors.white)),
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                  state.isShow
-                                      ? Formatter()
-                                          .formatCurrency(state.totalCredit)
-                                      : '***************',
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 17)),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                  state.isShow
-                                      ? Formatter()
-                                          .formatCurrency(state.totalDebit)
-                                      : '***************',
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 17)),
-                            ],
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Transaction',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              SizedBox(
-                height: 80,
-                width: double.infinity,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
+          return WillPopScope(
+            onWillPop: () async => false,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    GestureDetector(
-                      onTap: () => context
-                          .pushNamed(RouteConstants.creditor)
-                          .then((value) =>
-                              context.read<HomeCubit>().computeTotal()),
-                      child: Container(
-                        padding: const EdgeInsets.all(10),
+                    Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        color: kDefaultColor,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
                         child: Column(
                           children: [
-                            const Icon(CupertinoIcons.person_2_alt),
+                            Row(
+                              children: [
+                                Text(
+                                  'Hi ${state.fullname.toUpperCase()}!',
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                const Spacer(),
+                                IconButton(
+                                    onPressed: () {
+                                      context.read<HomeCubit>().toogleIsShow();
+                                    },
+                                    icon: Icon(
+                                      state.isShow
+                                          ? Icons.visibility_outlined
+                                          : Icons.visibility_off_outlined,
+                                      color: Colors.white,
+                                    ))
+                              ],
+                            ),
                             const SizedBox(
                               height: 10,
                             ),
-                            Text('Creditor',
-                                style: Theme.of(context).textTheme.labelSmall),
+                            Row(
+                              children: [
+                                const Column(
+                                  children: [
+                                    Text('Total Credit',
+                                        style: TextStyle(color: Colors.white)),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text('Total Debit',
+                                        style: TextStyle(color: Colors.white)),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                        state.isShow
+                                            ? Formatter().formatCurrency(
+                                                state.totalCredit)
+                                            : '***************',
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17)),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        state.isShow
+                                            ? Formatter().formatCurrency(
+                                                state.totalDebit)
+                                            : '***************',
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17)),
+                                  ],
+                                )
+                              ],
+                            ),
                           ],
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Text(
-                'Transaction History',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                  child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return const ListTile(
-                          leading: Icon(CupertinoIcons.person_alt, size: 30),
-                          title: Text('Creditor'),
-                          subtitle: Text('Date'),
-                          trailing: Text('P 1,000,000'),
-                        );
-                      },
-                      separatorBuilder: (context, index) => const Divider(),
-                      itemCount: 20))
-            ]),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Transaction',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: double.infinity,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          GestureDetector(
+                            onTap: () => context
+                                .pushNamed(RouteConstants.creditor)
+                                .then((value) =>
+                                    context.read<HomeCubit>().computeTotal()),
+                            child: Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  const Icon(CupertinoIcons.person_2_alt),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text('Creditor',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .labelSmall),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Text(
+                      'Transaction History',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Expanded(
+                        child: ListView.separated(
+                            itemBuilder: (context, index) {
+                              return const ListTile(
+                                leading:
+                                    Icon(CupertinoIcons.person_alt, size: 30),
+                                title: Text('Creditor'),
+                                subtitle: Text('Date'),
+                                trailing: Text('P 1,000,000'),
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                const Divider(),
+                            itemCount: 20))
+                  ]),
+            ),
           );
         },
       ),
